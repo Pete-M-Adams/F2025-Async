3a80e3d1c5b2cdea3b1fa2273f1a5bc4ac2401af
Ben <58788309+benji0295@users.noreply.github.com>
2025-10-19 15:52:10 -0400

    Updated search page to pair backend of TestSearch and frontend from SearchPage

--- frontend/cfyby/src/pages/Search.tsx
+++ frontend/cfyby/src/pages/Search.tsx
@@ -1,11 +1,43 @@
+import { Box, Typography } from "@mui/material";
 import Footer from "../components/Footer";
-import SearchSection from "../components/SearchSection"
+import LocationSearch from "../components/LocationSearch";
+import bgImage from "../assets/recordplayer.jpg";
 
 export default function Search() {
+
   return (
-    <div>
-      <SearchSection/>
+        <Box
+      sx={{
+        position: 'fixed', // take full viewport, regardless of parent
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100vh', // full screen height
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',  // vertical centering
+        alignItems: 'center',      // horizontal centering
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        zIndex: 1,
+      }}
+    >
+      {/* Title */}
+      <Typography
+        variant="h3"
+        sx={{
+          color: '#00aaff',
+          mb: 5,
+          fontWeight: 'bold',
+          textShadow: '1px 1px 2px rgba(0, 0, 0, 1)',
+        }}
+      >
+        Search By Location
+      </Typography>
+      <LocationSearch />
       <Footer />
-    </div>
+      </Box>
   );
 }
