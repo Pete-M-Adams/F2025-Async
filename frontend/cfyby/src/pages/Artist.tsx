62bf1de875e648fc3c7a8024ad2e617bdf785ed1
Emmet Allen <emmet.allen@gmail.com>
2025-11-06 18:46:36 -0500

    Added pre-lim mock for artist page

--- frontend/cfyby/src/pages/Artist.tsx
+++ frontend/cfyby/src/pages/Artist.tsx
@@ -4,6 +4,13 @@ function Artist() {
   return (
     <>
       <h1>Artist Page</h1>
+      <h2>Description</h2>
+      <div>
+        <p>List of Albums</p>
+        <p>Ratings for Each Album</p>
+      </div>
+      <h2>Albums or Songs</h2>
+      <h2>Ratings for Songs</h2>
     </>
   );
 }
