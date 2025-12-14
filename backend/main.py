988201e415865a09bae827aef74123ffe7c6a6dd
Pete Adams <peteadamsphl@gmail.com>
2025-12-14 15:44:38 -0500

    fixed pipe error in register artist

--- backend/main.py
+++ backend/main.py
@@ -486,8 +486,8 @@ class RegisteredArtist(BaseModel):
     genre: str
     name: str
     location: str
-    summary: str | None = None
-    image: str | None = None
+    summary: str or None = None
+    image: str or None = None
 
 @app.post("artists/register")
 async def register_artist(artist: RegisteredArtist):
